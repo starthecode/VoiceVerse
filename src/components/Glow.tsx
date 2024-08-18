7584981bf834b71a4d9475d934df6ec1cb8dfb64
Madan Malhotra <starthecode@gmail.com>
2024-08-18 18:07:03 +0530

    ignore

--- src/components/Glow.tsx
+++
@@ -1,7 +0,0 @@
-export const Glow = ({ classes }: any) => {
-  return (
-    <span
-      className={`${classes} fixed z-10 blur-[150px] w-[180px] h-[180px] rounded-full -translate-y-1/2 start-1/2 ltr:-translate-x-1/2 rtl:translate-x-1/2`}
-    />
-  );
-};
